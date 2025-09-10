8a52f9e2df72e05cd3a9ea638a64ee097f85ce71
elshan <elshan.elshan.82@gmail.com>
2025-09-10 16:25:06 +0400

    1

--- training.py
+++ training.py
@@ -1,29 +1,24 @@
-import paddle
-print(f"Paddle version: {paddle.__version__}")
-print(f"GPU available: {paddle.device.is_compiled_with_cuda()}")
-print(f"GPU count: {paddle.device.cuda.device_count()}")
-# # create_dict.py
-# def create_vocabulary(input_file, output_file):
-#     unique_chars = set()
-#
-#     with open(input_file, 'r', encoding='utf-8') as f:
-#         for line in f:
-#             parts = line.strip().split('\t')
-#             if len(parts) >= 2:
-#                 text = parts[1]
-#                 unique_chars.update(text)
-#
-#     sorted_chars = sorted(unique_chars)
-#
-#     with open(output_file, 'w', encoding='utf-8') as f:
-#         for char in sorted_chars:
-#             f.write(char + '\n')
-#
-#     print(f"Создан словарь: {output_file}")
-#     print(f"Найдено уникальных символов: {len(sorted_chars)}")
-#     print("Символы:", ''.join(sorted_chars))
-#
-#
-# input_file = 'text/typed_text/rec_gt.txt'  # Ваш файл с разметкой
-# output_file = 'text/typed_text/az_ru_dict.txt'  # Выходной файл словаря
-# create_vocabulary(input_file, output_file)
+def create_vocabulary(input_file, output_file):
+    unique_chars = set()
+
+    with open(input_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            parts = line.strip().split('\t')
+            if len(parts) >= 2:
+                text = parts[1]
+                unique_chars.update(text)
+
+    sorted_chars = sorted(unique_chars)
+
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for char in sorted_chars:
+            f.write(char + '\n')
+
+    print(f"Создан словарь: {output_file}")
+    print(f"Найдено уникальных символов: {len(sorted_chars)}")
+    print("Символы:", ''.join(sorted_chars))
+
+
+input_file = 'text/typed_text/rec_gt.txt'  # Ваш файл с разметкой
+output_file = 'text/typed_text/az_ru_dict.txt'  # Выходной файл словаря
+create_vocabulary(input_file, output_file)
