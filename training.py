b6199296c9f7e83c31deb3f3b9d89735c6ccd390
elshan <elshan.elshan.82@gmail.com>
2025-09-12 12:55:56 +0400

    Update training.py

--- training.py
+++ training.py
@@ -1,24 +1,46 @@
-def create_vocabulary(input_file, output_file):
-    unique_chars = set()
+# def create_vocabulary(input_file, output_file):
+#     unique_chars = set()
+#
+#     with open(input_file, 'r', encoding='utf-8') as f:
+#         for line in f:
+#             parts = line.strip().split('\t')
+#             if len(parts) >= 2:
+#                 text = parts[1]
+#                 unique_chars.update(text)
+#
+#     sorted_chars = sorted(unique_chars)
+#
+#     with open(output_file, 'w', encoding='utf-8') as f:
+#         for char in sorted_chars:
+#             f.write(char + '\n')
+#
+#     print(f"Создан словарь: {output_file}")
+#     print(f"Найдено уникальных символов: {len(sorted_chars)}")
+#     print("Символы:", ''.join(sorted_chars))
+#
+#
+# input_file = 'text/typed_text/rec_gt.txt'  # Ваш файл с разметкой
+# output_file = 'text/typed_text/az_ru_dict.txt'  # Выходной файл словаря
+# create_vocabulary(input_file, output_file)
+#
+# import os
+#
+#
+# def check_images_exist():
+#     with open('./text/typed_text/train_list.txt', 'r', encoding='utf-8') as f:
+#         lines = f.readlines()
+#
+#     for i, line in enumerate(lines):  # проверяем первые 10
+#         parts = line.strip().split('\t')
+#         if len(parts) >= 2:
+#             img_path = os.path.join('./text/typed_text', parts[0])
+#             if os.path.exists(img_path):
+#                 print(f"✓ {img_path}")
+#             else:
+#                 print(f"✗ {img_path} - НЕ СУЩЕСТВУЕТ!")
+#         else:
+#             print(f"✗ Неправильный формат: {line}")
+#
+#
+# check_images_exist()
 
-    with open(input_file, 'r', encoding='utf-8') as f:
-        for line in f:
-            parts = line.strip().split('\t')
-            if len(parts) >= 2:
-                text = parts[1]
-                unique_chars.update(text)
-
-    sorted_chars = sorted(unique_chars)
-
-    with open(output_file, 'w', encoding='utf-8') as f:
-        for char in sorted_chars:
-            f.write(char + '\n')
-
-    print(f"Создан словарь: {output_file}")
-    print(f"Найдено уникальных символов: {len(sorted_chars)}")
-    print("Символы:", ''.join(sorted_chars))
-
-
-input_file = 'text/typed_text/rec_gt.txt'  # Ваш файл с разметкой
-output_file = 'text/typed_text/az_ru_dict.txt'  # Выходной файл словаря
-create_vocabulary(input_file, output_file)
