dd33e698d0f42bab2c47f39af6ae00e2ff91a74f
elshan <elshan.elshan.82@gmail.com>
2025-10-08 12:16:49 +0400

    Update testFast.py

--- testFast.py
+++ testFast.py
@@ -1,16 +1,26 @@
 from paddleocr import *
 
 
-def PRec(image):
+def PRec_old(image):
+    ocr = PaddleOCR()
+    result = ocr.predict(image)
+    return result
+def PRec_new(image):
     ocr = PaddleOCR(text_recognition_model_dir="./output/best_accuracy")
     result = ocr.predict(image)
-    ocr
     return result
 
 
 def main():
     image_path = 'text/debug/img1.jpg'
-    result = PRec(image_path)
+    result = PRec_old(image_path)
+    # save_txt(result, filename='temp1.txt')
+    if result and len(result) > 0:
+        page = result[0]
+        texts = page.get('rec_texts', [])
+        combined_text = ' '.join(texts)
+    print(combined_text)
+    result = PRec_new(image_path)
     # save_txt(result, filename='temp1.txt')
     if result and len(result) > 0:
         page = result[0]
