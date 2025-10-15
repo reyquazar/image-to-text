d8cbd61dc445b0c2b25a89f73d0c169251374e19
elshan <elshan.elshan.82@gmail.com>
2025-10-15 13:01:59 +0400

    Update checkpretrain.py

--- checkpretrain.py
+++ checkpretrain.py
@@ -1,4 +1,5 @@
 import paddle
+
 params = paddle.load('./text/typed_text/pretrain_models/PP-OCRv5_server_rec_pretrained.pdparams')
 print('Keys:', list(params.keys())[:5])
 print('Total layers:', len(params))
