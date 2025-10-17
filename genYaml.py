cccb48dd2d238ee28eb23cf0a6e4d6c8e87544e5
elshan <elshan.elshan.82@gmail.com>
2025-10-17 16:35:28 +0400

    Create genYaml.py

---
+++ genYaml.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+base_path = "./text/typed_text/az_config_train"
+subprocess.run([
+    "python", "../PaddleOCR/configs/rec/mult_language/generate_multi_language_configs.py",
+    "-l", "az",
+    "--dict", f"{base_path}/dict.txt",
+    "--train", f"{base_path}/train_list.txt",
+    "--val", f"{base_path}/val_list.txt",
+    "--data_dir", base_path
+])
