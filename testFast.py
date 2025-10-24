a241a0fc00105ba4111fe0c0b08e3d39a3ab7952
elshan <elshan.elshan.82@gmail.com>
2025-10-24 14:47:23 +0400

    Update testFast.py

--- testFast.py
+++ testFast.py
@@ -15,7 +15,7 @@ def PRec_old(image):
 
 def PRec_new(image):
     ocr = PaddleOCR(
-        text_recognition_model_name="./output/inference/",
+        text_recognition_model_dir="./output/inference/",
         use_doc_orientation_classify=False,
         use_textline_orientation=False,
     )
