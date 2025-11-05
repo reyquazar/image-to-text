242f36fad409bda412578016a5b8fc6107d5f3c6
elshan <elshan.elshan.82@gmail.com>
2025-11-05 14:42:14 +0400

    Update testFast.py

--- testFast.py
+++ testFast.py
@@ -49,7 +49,7 @@ def main():
     printL("Foutput1", image_path)  # synt data with gDTV2Words.py
     printL("Foutput2", image_path)  # synt data with gDTV2Words.py
     printL("Foutput3", image_path)  # synt data with gDTV2Words2.py
-    PRec(image_path)  # latest
+    printL("output", image_path)  # synt data with gDTV2Words2.py
 
 
 if __name__ == "__main__":
