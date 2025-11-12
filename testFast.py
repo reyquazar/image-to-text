45671db5f21a5d2c38e5a64be056e15bd18aca02
elshan <elshan.elshan.82@gmail.com>
2025-11-12 17:05:06 +0400

    Update testFast.py

--- testFast.py
+++ testFast.py
@@ -20,7 +20,7 @@ def PRec(image):
     print("=" * 100)
 
 
-def printL(model, image):
+def printL(model, image, text):
     ocr = PaddleOCR(
         text_recognition_model_dir=f"./{model}/inference/",
         use_doc_orientation_classify=False,
@@ -32,7 +32,7 @@ def printL(model, image):
         texts = page.get('rec_texts', [])
         combined_text = ' '.join(texts)
     print("=" * 150)
-    print(model)
+    print(model, text)
     print("=" * 150)
     print(combined_text)
     print("=" * 150)
@@ -46,14 +46,14 @@ def main():
     # image_path = './text/typed_text/testWords.png'
 
     PRec(image_path)  # default paddle model
-    printL("output1", image_path)  # synt data with gDTV2Words.py
-    printL("output2", image_path)  # synt data with gDTV2Words.py
-    printL("output3", image_path)  # synt data with gDTV2Words2.py
-    printL("output4", image_path)  # synt data with gDTV2Words2.py
-    printL("output5", image_path)  # synt data with gDTV2Words3.py
-    printL("output6", image_path)  # synt data with gDTV2Words3.py
-    printL("output7", image_path)  # hw + copy do 18.png
-    printL("output", image_path)  # hw + copy all.png
+    printL("output1", image_path, "synt data with gDTV2Words.py 100k Train 10k Val")  # synt data with gDTV2Words.py
+    printL("output2", image_path, "synt data with gDTV2Words.py 100k Train 10k Val")  # synt data with gDTV2Words.py
+    printL("output3", image_path, "synt data with gDTV2Words2.py 200k Train 20k Val")  # synt data with gDTV2Words2.py
+    printL("output4", image_path, "synt data with gDTV2Words2.py 200k Train 20k Val")  # synt data with gDTV2Words2.py
+    printL("output5", image_path, "synt data with gDTV2Words3.py 100k Train 10k Val")  # synt data with gDTV2Words3.py
+    printL("output6", image_path, "synt data with gDTV2Words3.py 100k Train 10k Val")  # synt data with gDTV2Words3.py
+    printL("output7", image_path, "hw original + test1-18.png")  # hw + copy do 18.png
+    printL("output", image_path, "hw original + test1-22.png")  # hw + copy all.png
 
 
 if __name__ == "__main__":
