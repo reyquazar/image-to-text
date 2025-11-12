a9f9b44b063647bb833dc1f83b6c43a1c5610c36
elshan <elshan.elshan.82@gmail.com>
2025-11-12 12:36:56 +0400

    Update testFast.py

--- testFast.py
+++ testFast.py
@@ -45,14 +45,15 @@ def main():
     image_path = args.image_path
     # image_path = './text/typed_text/testWords.png'
 
-    PRec(image_path)  # default paddle model
-    printL("output1", image_path)  # synt data with gDTV2Words.py
-    printL("output2", image_path)  # synt data with gDTV2Words.py
-    printL("output3", image_path)  # synt data with gDTV2Words2.py
-    printL("output4", image_path)  # synt data with gDTV2Words2.py
-    printL("output5", image_path)  # synt data with gDTV2Words3.py
-    printL("output6", image_path)  # synt data with gDTV2Words3.py
-    printL("output", image_path)  # synt data with gDTV2Words3.py + hw
+    # PRec(image_path)  # default paddle model
+    # printL("output1", image_path)  # synt data with gDTV2Words.py
+    # printL("output2", image_path)  # synt data with gDTV2Words.py
+    # printL("output3", image_path)  # synt data with gDTV2Words2.py
+    # printL("output4", image_path)  # synt data with gDTV2Words2.py
+    # printL("output5", image_path)  # synt data with gDTV2Words3.py
+    # printL("output6", image_path)  # synt data with gDTV2Words3.py
+    # printL("output7", image_path)  # hw + copy
+    printL("output", image_path)  # hw + copy
 
 
 if __name__ == "__main__":
