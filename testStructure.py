d9129f3ad703b91e9f42a155d6c6a8aa1515ad4f
elshan <elshan.elshan.82@gmail.com>
2025-11-17 10:32:19 +0400

    Update testStructure.py

--- testStructure.py
+++ testStructure.py
@@ -59,15 +59,17 @@
 
 from paddleocr import PPStructureV3
 
-pipeline = PPStructureV3()
+# pipeline = PPStructureV3()
 # pipeline = PPStructureV3(lang="en") # Set the lang parameter to use the English text recognition model. For other supported languages, see Section 5: Appendix. By default, both Chinese and English text recognition models are enabled.
 # pipeline = PPStructureV3(use_doc_orientation_classify=True) # Use use_doc_orientation_classify to enable/disable document orientation classification model
 # pipeline = PPStructureV3(use_doc_unwarping=True) # Use use_doc_unwarping to enable/disable document unwarping module
 # pipeline = PPStructureV3(use_textline_orientation=True) # Use use_textline_orientation to enable/disable textline orientation classification model
-# pipeline = PPStructureV3(device="gpu") # Use device to specify GPU for model inference
+pipeline = PPStructureV3(device="gpu",
+                         text_recognition_model_dir="./output2/inference/",
+                         )  # Use device to specify GPU for model inference
 img_path = "./img_3.png"
 output = pipeline.predict(img_path)
 for res in output:
-    res.print() ## Print the structured prediction output
-    res.save_to_json(save_path="output") ## Save the current image's structured result in JSON format
-    res.save_to_markdown(save_path="output") ## Save the current image's result in Markdown format
+    res.print()  ## Print the structured prediction output
+    res.save_to_json(save_path="output")  ## Save the current image's structured result in JSON format
+    res.save_to_markdown(save_path="output")  ## Save the current image's result in Markdown format
